2a9d889542d2a283276581698c5499a26010bf9c
srelard <41011720+srelard@users.noreply.github.com>
2020-10-04 16:35:23 +0200

    test

--- lib/main.dart
+++ lib/main.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'You have pushed the button this noooo times:',
             ),
             Text(
               '$_counter',
@@ -110,7 +110,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: Icon(Icons.access_alarms_outlined),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
