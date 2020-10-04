7aefaa9dce89a36994552cc5b1f32ce98c77f003
srelard <41011720+srelard@users.noreply.github.com>
2020-10-04 19:23:48 +0200

    init

--- lib/main.dart
+++ lib/main.dart
@@ -68,7 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         label: Text('Approve'),
         icon: Icon(Icons.thumb_up),
-        backgroundColor: Colors.pink,
+        backgroundColor: Colors.amber,
       ),
     );
   }
